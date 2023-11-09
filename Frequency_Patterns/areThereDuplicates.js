e7b874c0b4db8cccaa9288b4c7cca7ba818e7006
Manoj Mishra <manojmishra98feb@gmail.com>
2023-11-09 20:26:21 +0530

    till part 1 completion

---
+++ Frequency_Patterns/areThereDuplicates.js
@@ -0,0 +1,45 @@
+
+let arr = [1,2]
+function areThereDuplicates(arr) {
+  // good luck. (supply any arguments you deem necessary.)
+      let frequencyObj ={};
+  for (let val of arr) {
+      frequencyObj[val] = (frequencyObj[val] || 0) + 1;
+  }
+  
+ for (var key in frequencyObj) {
+     if(frequencyObj[key]>1){
+         console.log(key)
+         // return true;
+         
+     }
+ }
+ 
+ // return false; 
+}
+
+console.log(areThereDuplicates(arr));
+
+
+// 2nd approch
+
+function areThereDuplicates() {
+    let collection = {}
+    for(let val in arguments){
+      collection[arguments[val]] = (collection[arguments[val]] || 0) + 1
+    }
+    for(let key in collection){
+      if(collection[key] > 1) return true
+    }
+    return false;
+  }
+
+  // linnear one
+
+  function areThereDuplicates() {
+     let newArr = new Set(arguments);
+     if(newArr.size != arguments.length){
+        return false;
+     }
+     return true;
+  }
---
+++ Frequency_Patterns/sameFrequency.js
@@ -0,0 +1,36 @@
+function sameFrequency(num1,num2){
+
+    const num1S = String(num1);
+     const num2S = String(num2);
+ 
+     if (num1S.length !== num2S.length ) {
+         return false;
+     }
+ 
+     let freqnum1={};
+     let freqnum2={};
+    
+       for(const val of num1S){
+         
+         freqnum1[val] = (freqnum1[val]||0)+1;
+     }
+     for(const val of num2S){
+         
+         freqnum2[val] = (freqnum2[val]||0)+1;
+     }
+ 
+    for (let key in freqnum1) {
+ 
+        console.log(freqnum1[key])
+         console.log( freqnum2[key])
+        if (freqnum1[key] !== freqnum2[key]) {
+           return false; 
+        }
+    }
+ 
+ 
+     return true;
+     
+ }
+ 
+ console.log(sameFreq(102,102));
