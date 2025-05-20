ff7f20f9540a57b672883c3a42e74ec467ee8d95
Manoj mishra <manojmishra98feb@gmail.com>
2025-05-20 10:49:04 +0530

    Till  max depth completed

--- Strings/maxNestDepth.js
+++ Strings/maxNestDepth.js
@@ -1,14 +1,24 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
 var maxDepth = function(s) {
+
     let count = 0;
-    let maxNum = 0;
-    for (let c of s) {
-        if (c === '(') {
-            count++;
-            if (maxNum < count)
-                maxNum = count;
-        } else if (c === ')') {
+    let max = 0;
+    for(let i=0;i<s.length;i++){
+        // check if char is (  then increase the count
+        if(s[i]=="("){
+           count++;
+           // always take the max of count for ( by comparing with count & max
+           max = Math.max(count,max);
+        }
+        // if we got ) then simply decrease the count by 1 to travel through all the char in the string
+        else if(s[i]==")"){
             count--;
         }
     }
-    return maxNum;
+
+    return max;
+    
 };
--- Strings/validAnagram.js
+++ Strings/validAnagram.js
@@ -31,3 +31,42 @@ var isAnagram = function(string,target) {
 
 
  console.log(isAnagram("manoj","joman"))
+
+
+ // optimized approach
+
+ /**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isIsomorphic = function(s, t) {
+
+    // check if the length of both string equal or not if not return false
+        if(s.length !==t.length){
+            return false;
+        }
+        // create two hashmap one for S one for T
+        const mapS = new Map();
+        const mapT = new Map();
+      // itreate through the string and calulate each char from s and t
+        for(let i=0;i<s.length;i++){
+             const charS = s[i];
+             const charT = t[i];
+          // check if the key value pair is matching or not for both map
+          // if any where it is not matching then return false
+          // we have set for map s key as s char and val as t char
+          // same for map t chart as key and charS as value
+             if(mapS.has(charS)&& mapS.get(charS)!== charT ||
+             mapT.has(charT)&& mapT.get(charT)!== charS){
+                return false;
+             }
+             // set the char t and s to both map 
+             // for t ---> t,s
+             // for s --> s,t
+             mapS.set(charS,charT);
+             mapT.set(charT,charS);
+        }
+        return true;
+        
+    };
